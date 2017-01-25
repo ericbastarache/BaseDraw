5a799d6118a726ee48113215d212ff588ca287ba
ericb <eric_bastarache@hotmail.com>
2017-01-24 21:59:41 -0500

    added color to each button on load

--- js/draw.js
+++ js/draw.js
@@ -1,20 +1,32 @@
-var color = document.getElementById('colorChoice').value;
-var choice = document.getElementById('choice');
+var canvas = document.getElementById('drawing-board');
+canvas.width = 1200;
+canvas.height = 850;
+var ctx = canvas.getContext('2d');
 
 window.onload = function () {
-  color = document.getElementById('colorChoice').value;
-  choice.innerHTML = color;
-
-  var canvas = document.getElementById('drawing-board');
-  var ctx = canvas.getContext('2d');
+  
+  //Apply the background color to the buttons
+  for(var i = 0; i < document.getElementsByClassName('btn-color').length; i++) {
+    document.getElementsByClassName('btn-color')[i].style.background = document.getElementsByClassName('btn-color')[i].value;
+  }
+  draw();
 }
 
-document.getElementById('colorChoice').addEventListener('click', function () {
-  color = document.getElementById('colorChoice').value;
-  choice.innerHTML = color;
+var choiceButton = document.getElementById('colorChoice').addEventListener('click', function () {
+  ctx.fillStyle = document.getElementById('colorChoice').value;
 });
 
-document.getElementById('colorChoice').addEventListener('change', function () {
-  color = document.getElementById('colorChoice').value;
-  choice.innerHTML = color;
+var choiceButtonChange = document.getElementById('colorChoice').addEventListener('change', function () {
+  ctx.fillStyle = document.getElementById('colorChoice').value;
+  draw();
 });
+
+var redButton = document.getElementById('red-button').addEventListener('click', function () {
+  document.getElementById('red-button').style.background = document.getElementById('red-button').value;
+  ctx.fillStyle = document.getElementById('red-button').value;
+});
+
+function draw () {
+  ctx.fillStyle = document.getElementById('colorChoice').value;
+  ctx.fillRect(10,10,50,50);
+}
