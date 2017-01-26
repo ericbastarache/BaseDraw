a101aded6fc1451df84a42206cb7071f094591a3
ericb <eric_bastarache@hotmail.com>
2017-01-26 16:34:28 -0500

    added support for drawing a heart

--- js/draw.js
+++ js/draw.js
@@ -72,7 +72,15 @@ function drawShape(elem) {
     ctx.fillRect(30,30,100,100);
   }
   if(elem.id === "heart") {
-    ctx.fillRect(60,60, 100, 100);
+    ctx.beginPath();
+    ctx.moveTo(75, 40);
+    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+    ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+    ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+    ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+    ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+    ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+    ctx.fill();
   }
   if(elem.id === "line") {
     ctx.fillRect(90,90,100,100);
