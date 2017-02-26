d66827de9ddaac3e83e8c816e998eba3851e9a18
ericb <eric_bastarache@hotmail.com>
2017-02-26 04:02:25 -0500

    Finished basic drawing application

--- js/draw.js
+++ js/draw.js
@@ -4,9 +4,33 @@ canvas.height = 850;
 let ctx = canvas.getContext('2d');
 
 //Variables for download link and download element
-var dl, dlElem;
+let dl, dlElem;
+
+//Variables for tracking mouse location
+let flag = false;
+
+let prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0;
 
 window.onload = () => {
+
+  canvas.addEventListener('mousemove', (e) => {
+    findxy('move', e);
+  }, false);
+
+  canvas.addEventListener('mousedown', (e) => {
+    findxy('down', e);
+  }, false);
+
+  canvas.addEventListener('mouseup', (e) => {
+    findxy('up', e);
+  }, false);
+
+  canvas.addEventListener('mouseout', (e) => {
+    findxy('out', e);
+  }, false);
 
   //Apply the background color to the buttons
   for(let i = 0; i < document.getElementsByClassName('btn-color').length; i++) {
@@ -30,11 +54,6 @@ document.getElementById('colorChoice').addEventListener('change', () => {
   document.getElementById('choice').style.background = document.getElementById('colorChoice').value;
 });
 
-document.getElementById('toolbox').addEventListener('click', (e) => {
-  drawShape(e.target);
-});
-
-
 document.getElementById('clear-button').addEventListener('click', () => {
   document.getElementById('download').style = "display: none;";
   document.getElementById('save-button').disabled = false;
@@ -56,12 +75,48 @@ document.getElementById('save-button').addEventListener('click', (e) => {
   this.disabled = true;
 });
 
-var draw = () => {
-  ctx.fillStyle = document.getElementById('colorChoice').value;
+const findxy = (res, e) => {
+  if(res == 'down') {
+    prevX = currX;
+    prevY = currY;
+    currX = e.clientX - canvas.offsetLeft;
+    currY = e.clientY - canvas.offsetTop;
+
+    flag = true;
+
+    ctx.beginPath();
+    ctx.fillStyle = document.getElementById('colorChoice').value;
+    ctx.fillRect(currX, currY, 2, 2);
+    ctx.closePath();
+
+  }
+
+  if(res == 'up' || res == 'out') {
+    flag = false;
+  }
+
+  if(res == 'move') {
+    if(flag) {
+      prevX = currX;
+      prevY = currY;
+      currX = e.clientX - canvas.offsetLeft;
+      currY = e.clientY - canvas.offsetTop;
+      draw();
+    }
+  }
+}
+
+const draw = () => {
+  ctx.strokeStyle = document.getElementById('colorChoice').value;
+  ctx.beginPath();
+  ctx.moveTo(prevX, prevY);
+  ctx.lineTo(currX, currY);
+  ctx.stroke();
+  ctx.closePath();
 
 }
 
-var changeColor = (elem) => {
+const changeColor = (elem) => {
   if(elem.value === 0) {
     document.getElementById('colorChoice').value = "#000000";
     document.getElementById('choice').style.background = "#000000";
@@ -69,93 +124,5 @@ var changeColor = (elem) => {
     ctx.fillStyle = elem.value;
     document.getElementById('colorChoice').value = elem.value;
     document.getElementById('choice').style.background = elem.value;
-  }
-}
-
-var drawShape = (elem) => {
-  if(elem.id === "gradient") {
-    if (document.getElementById('gradient-tool').style = "display: none") {
-      document.getElementById('gradient-tool').style = "display: block";
-    }
-  }
-  if(elem.id === "square") {
-    if(document.getElementById('brush-tool').style = "display: block") {
-      document.getElementById('brush-tool').style = "display: none";
-    }
-    ctx.fillRect(300, 300, 100,100);
-  }
-  if(elem.id === "callout") {
-    if(document.getElementById('brush-tool').style = "display: block") {
-      document.getElementById('brush-tool').style = "display: none";
-    }
-    ctx.beginPath();
-    ctx.moveTo(75, 25);
-    ctx.quadraticCurveTo(25, 25, 25, 62.5);
-    ctx.quadraticCurveTo(25, 100, 50, 100);
-    ctx.quadraticCurveTo(50, 120, 30, 125);
-    ctx.quadraticCurveTo(60, 120, 65, 100);
-    ctx.quadraticCurveTo(125, 100, 125, 62.5);
-    ctx.quadraticCurveTo(125, 25, 75, 25);
-    ctx.stroke();
-  }
-  if(elem.id === "circle") {
-    if(document.getElementById('brush-tool').style = "display: block") {
-      document.getElementById('brush-tool').style = "display: none";
-    }
-    let circle = new Path2D();
-    circle.moveTo(125, 35);
-    circle.arc(100, 35, 25, 0, 2 * Math.PI);
-    ctx.fill(circle);
-  }
-  if(elem.id === "heart") {
-    if(document.getElementById('brush-tool').style = "display: block") {
-      document.getElementById('brush-tool').style = "display: none";
-    }
-    ctx.beginPath();
-    ctx.moveTo(75, 40);
-    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
-    ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
-    ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
-    ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
-    ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
-    ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
-    ctx.fill();
-  }
-  if(elem.id === "line") {
-    if(document.getElementById('brush-tool').style = "display: block") {
-      document.getElementById('brush-tool').style = "display: none";
-    }
-    ctx.fillRect(90,90,100,100);
-  }
-  if(elem.id === "star") {
-    if(document.getElementById('brush-tool').style = "display: block") {
-      document.getElementById('brush-tool').style = "display: none";
-    }
-    let points = [ [ 0, 85 ], [ 75, 75 ], [ 100, 10 ], [ 125, 75 ],
-    [ 200, 85 ], [ 150, 125 ], [ 160, 190 ], [ 100, 150 ],
-    [ 40, 190 ], [ 50, 125 ], [ 0, 85 ] ];
-
-    let len = points.length;
-
-    ctx.beginPath();
-    ctx.moveTo(points[0][0], points[0][1]);
-
-    for (let i = 0; i < len; i++) {
-      ctx.lineTo(points[i][0], points[i][1]);
-    }
-
-    ctx.fill();
-  }
-  if(elem.id === "pencil") {
-    if(document.getElementById('brush-tool').style = "display: none") {
-      document.getElementById('brush-tool').style = "display: block";
-    }
-  }
-  if(elem.id === "text") {
-    if(document.getElementById('brush-tool').style = "display: block") {
-      document.getElementById('brush-tool').style = "display: none";
-    }
-    ctx.font = "48px serif";
-    ctx.fillText('Hello, my name is Hello World!', canvas.width/4, canvas.height/2);
   }
 }
